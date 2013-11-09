cdf9344d8da50812e47af83df81618d84a2da0e3
Vincent Povirk <madewokherd@gmail.com>
2013-11-09 11:36:42 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <Windows.h>
+
+#include "resource.h"
+
+INT_PTR CALLBACK MainDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		EndDialog(hwndDlg, 0);
+		break;
+	}
+
+	return FALSE;
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOGMAIN), NULL, MainDialogProc);
+
+	return 0;
+}
